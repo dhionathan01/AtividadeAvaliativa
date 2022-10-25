0755464daed9dbdecebfe9ee66befe0cb56a53c8
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-10-24 23:34:06 -0300

    Subindo Trabalho

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {Montserrat_400Regular, Montserrat_700Bold, useFonts,} from '@expo-google-fonts/montserrat';
+import AppLoading  from 'expo-app-loading';
+
+import Cesta from './src/telas/Cesta';
+
+{/* Importa o mock*/}
+import mock from './src/mocks/cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <AppLoading/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView>
+        <StatusBar/>
+        {/* Passa o mock para o componente ...-> server para descontruir o objeto*/}
+        {/* igual a dizer -> topo ={mock.topo}, detalhes = {mock.detalhes}*/}
+        <Cesta {...mock} />
+      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
